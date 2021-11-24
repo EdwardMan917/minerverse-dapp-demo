8266ce59414159ad436649e694d80e0eb72a2950
edwardman <edwardman917@gmail.com>
2021-11-25 00:05:34 +0800

    conf: add gh-pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import MainFrame from './components/MainFrame';
 
-import { ethers } from "ethers";
+// import { ethers } from "ethers";
 
-async function connect(){
-  const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
-  // Prompt user for account connections
-  await provider.send("eth_requestAccounts", []);
-  const signer = provider.getSigner();
-  console.log("Account:", await signer.getAddress());
-}
+// async function connect(){
+//   const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+//   // Prompt user for account connections
+//   await provider.send("eth_requestAccounts", []);
+//   const signer = provider.getSigner();
+//   console.log("Account:", await signer.getAddress());
+// }
 
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
 
   // // There is only ever up to one account in MetaMask exposed
   // const signer = provider.getSigner();
-  connect();
+  // connect();
 
   return (
     <MainFrame />
