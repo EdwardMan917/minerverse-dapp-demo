686fff26c9f6012c3c18a21b73c58953285e4397
EdwardVsys1 <edward.vsys1@gmail.com>
2021-12-16 11:03:52 +0800

    style: differenciate grey colors

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -3,5 +3,7 @@ export enum Colors {
   NavBorderGrey = "#313131",
   Black = "#000",
   White = "#ffffff",
-  RadiationGrey = "#535353"
+  RadiationGrey = "#535353",
+  FormBorderGrey = "#3d3d3d",
+  AddressBoxGrey = "#232323"
 }
